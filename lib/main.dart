5f27f93aa195e1b6162a74ac71ee87df23ea50fe
rushannanayakkara <rushannana@gmail.com>
2020-05-04 19:39:28 +0530

    complete project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() => runApp(home());
+
+class home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  int imageNo = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[700],
+        appBar: AppBar(
+          title: Center(child: Text("Ask Me Anything")),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+              onPressed: this.changeImage,
+              child: Image.asset("images/ball" + imageNo.toString() + ".png"),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  void changeImage() {
+    setState(() {
+      imageNo = Random().nextInt(4) + 1;
+    });
+  }
+}
